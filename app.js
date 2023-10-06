664a19e7b8c32d54d821f5d49f80989e75ad013a
dieuhan <dieuhan.mut@gmail.com>
2023-10-06 13:12:12 -0400

    i am learning

--- app.js
+++ app.js
@@ -1,8 +1,33 @@
-const song = {
-    Name: "Golden",
+const song = [
+
+ {  Name: "Golden", 
     Artist: "Harry Styles",
     Key: "E minor",
     BPM: "140"
-};
+ },
 
-console.log(song);
+ {
+    Name: "Style",
+    Artist: "Taylor Swift",
+    Key: "D major",
+    BPM: "95"
+ },
+ 
+ {
+    Name: "deja vu",
+    Artist: "Olivia Rodrigo",
+    Key: "A major",
+    BPM: "181",
+ },
+];
+
+song.forEach((song)=> console.log(song.Name));
+
+   
+//const number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+//numbers.forEach((song, index)=> console.log(song, index))
+
+// const numbers is bc index starts at 0
+// arrow function ()=> auto returns output, keyword points global
+// forEach is a loop 
+
